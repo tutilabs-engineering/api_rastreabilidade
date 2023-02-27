7214d71a7e74858b584f1cb223531d5ab7fb7217
paulabssantos <paulablsantos7@gmail.com>
2023-02-27 11:31:37 -0400

    feat: criacao de rota de listagem de usuario por id

--- src/shared/routes/user.routes.ts
+++ src/shared/routes/user.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { FindUserByIdController } from "../../modules/User/useCases/FindUserByIdUseCase/FindUserByIdController";
 import { ListUserController } from "../../modules/User/useCases/ListUserUseCase/ListUserController";
 
 const listUserController = new ListUserController()
+const findUserByIdController = new FindUserByIdController()
+
 const userRouter = Router()
 
 userRouter.get("/",listUserController.handle)
+userRouter.get("/:id",findUserByIdController.handle)
 
 export { userRouter }
