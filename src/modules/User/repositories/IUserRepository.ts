96da9d5af12741b9eda6f63f9db7af2cf1c2fa01
paulabssantos <paulablsantos7@gmail.com>
2023-02-27 12:37:39 -0400

    fix: atualizacao parametros metodo de atualizacao

--- src/modules/User/repositories/IUserRepository.ts
+++ src/modules/User/repositories/IUserRepository.ts
@@ -3,7 +3,7 @@ import { User } from "../entities/User"
 export interface IUserRepository{
     create():Promise<void>
     list():Promise<User[]>
-    update():Promise<void>
+    update(id: string, userInfo: IUpdateUserDTO):Promise<void>
     delete():Promise<void>
     findById(id: string):Promise<User | null>;
 }
