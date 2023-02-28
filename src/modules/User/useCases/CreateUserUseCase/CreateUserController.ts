1501fa2916e4aaa5d5165a627c78ae3f1183e32a
paulabssantos <paulablsantos7@gmail.com>
2023-02-28 10:37:07 -0400

    feat: criacao de um usuario

---
+++ src/modules/User/useCases/CreateUserUseCase/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+
+
+class CreateUserController{
+    async handle(req: Request, res: Response){
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        const {nome, matricula, email, password, admin, mnt, ativo} = req.body
+        
+        await createUserUseCase.execute({nome,ativo,admin,mnt,email,matricula,password})
+
+        return res.status(200).json({message: "Usuário criado com sucesso!"})
+    }
+}
+
+export { CreateUserController }
---
+++ src/modules/User/useCases/CreateUserUseCase/CreateUserUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from "tsyringe";
+import { HashPassword } from "../../../../utils/HashPassword";
+import { ValidarEmail } from "../../../../utils/ValidarEmail";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+import utc from 'dayjs/plugin/utc'
+import dayjs from "dayjs";
+
+@injectable()
+class CreateUserUseCase{
+    constructor(@inject("UserRepository") private userRepository: IUserRepository){}
+
+    async execute({nome,ativo,admin, mnt,email,matricula,password}:  ICreateUserDTO){
+        
+        if(!nome) {throw new Error("Nome não existe, informe um nome")}
+
+        if(!email) {throw new Error("Email não existe, informe um email")}
+        const emailValido = ValidarEmail(email);
+        if(!emailValido) {throw new Error("Email inválido, informe um novo email")}
+
+        if(!password) {throw new Error("Senha vazia, insira uma senha")}
+        if(password.length < 6) {throw new Error("Senha muito curta, insira uma nova senha")}
+
+        const emailExists = await this.userRepository.findUserByEmail(email);
+        if(emailExists){
+            throw new Error("Usuário já existe, informe um novo email");
+        }
+
+        const matriculaExists = await this.userRepository.findUserByMatricula(matricula);
+        if(matriculaExists){
+            throw new Error("Usuário com esta matrícula já existe");
+        }
+        
+        dayjs.extend(utc)
+        const createdAt = new Date(dayjs().utc(true).toISOString());
+        const updatedAt = new Date(dayjs().utc(true).toISOString());
+
+        
+        const hash_password = await HashPassword(password);
+
+        await this.userRepository.create({admin, mnt, ativo, email,matricula,nome,password: hash_password,createdAt,updatedAt})
+    }
+}
+
+export { CreateUserUseCase }
