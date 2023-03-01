bed7aece9af83b45fa20db10a8c55fa9619b3e53
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 11:14:46 -0400

    feat: criacao de fornecedor

--- src/modules/Provider/useCases/CreateProviderUseCase/CreateProviderUseCase.ts
+++ src/modules/Provider/useCases/CreateProviderUseCase/CreateProviderUseCase.ts
@@ -1,4 +1,8 @@
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc'
+
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../config/AppError";
 import { CreateProviderDTO } from "../../dtos/CreateProviderDTO";
 import { IProviderRepository } from "../../repository/IProviderRepository";
 
@@ -12,7 +16,19 @@ class CreateProviderUseCase {
     ){}
 
     async execute({nome, externo}: CreateProviderDTO): Promise<void>{
+        dayjs.extend(utc)
 
+        if(!nome){
+            throw new AppError(400, "Nome não pode ficar vazio.")
+        }
+
+        if(externo == null){
+            throw new AppError(400, "Local não pode ficar vazio")
+        }
+        const createdAt = new Date(dayjs().utc(true).toISOString());
+        const updatedAt = new Date(dayjs().utc(true).toISOString());
+
+        await this.providerRepository.create({nome,externo,createdAt,updatedAt})
     }
 
 
