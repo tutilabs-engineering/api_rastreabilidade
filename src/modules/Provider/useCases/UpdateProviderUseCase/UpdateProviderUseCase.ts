98af1b1e32a088d7f5b2defe189baead690f4749
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 13:54:51 -0400

    feat: atualizar fornecedor

--- src/modules/Provider/useCases/UpdateProviderUseCase/UpdateProviderUseCase.ts
+++ src/modules/Provider/useCases/UpdateProviderUseCase/UpdateProviderUseCase.ts
@@ -1,4 +1,8 @@
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc'
+
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../config/AppError";
 import {  UpdateProviderDTO } from "../../dtos/UpdateProviderDTO";
 import { IProviderRepository } from "../../repository/IProviderRepository";
 
@@ -10,9 +14,21 @@ class UpdateProviderUseCase {
     ){}
 
     async execute({id,nome, externo}: UpdateProviderDTO) {
-  
-          
-    
+
+        dayjs.extend(utc)
+        const createdAt = new Date(dayjs().utc(true).toISOString());
+        const updatedAt = new Date(dayjs().utc(true).toISOString());
+        
+        const provider = await this.providerRepository.findById({id});
+          if(!provider){ 
+            throw new AppError(404,"Fornecedor não existe no banco.")
+          }
+
+          if(!nome){
+            nome = String(provider.nome)    
+        }
+        
+        await this.providerRepository.update({id,externo,nome,updatedAt,createdAt})
 
       }
 
