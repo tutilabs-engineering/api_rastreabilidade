1494f40b86c13be2eb9019544ded5ad8f4e01266
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 13:57:33 -0400

    feat: rota de atualizacao de fornecedor

--- src/shared/routes/provider.routes.ts
+++ src/shared/routes/provider.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { CreateProviderController } from "../../modules/Provider/useCases/CreateProviderUseCase/CreateProviderController";
 import { ListProviderController } from "../../modules/Provider/useCases/ListProviderUseCase/ListProviderController";
+import { UpdateProviderController } from "../../modules/Provider/useCases/UpdateProviderUseCase/UpdateProviderController";
 
 
 const providerRouter = Router()
 
 const listProviderController = new ListProviderController()
 const createProviderController = new CreateProviderController()
+const updateProviderController = new UpdateProviderController()
 
 providerRouter.get("/",listProviderController.handle)
 providerRouter.post("/",createProviderController.handle)
+providerRouter.put("/:id",updateProviderController.handle)
 
 export { providerRouter }
