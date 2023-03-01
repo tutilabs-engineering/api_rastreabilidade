6eb1112d95d218944d45e4dc9119ee5ab71525a4
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 09:10:11 -0400

    feat: rota de atualizacao de embalagem

--- src/shared/routes/package.routes.ts
+++ src/shared/routes/package.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { FindBySerialNumberController } from "../../modules/Package/useCase/FindBySerialNumberUseCase/FindBySerialNumberController";
+import { UpdatePackageController } from "../../modules/Package/useCase/UpdatePackageUseCase/UpdatePackageController";
 
 
 const packageRouter = Router()
 const findBySerialNumberController = new FindBySerialNumberController()
+const updatePackageController = new UpdatePackageController()
 
 packageRouter.get("/:serial_number", findBySerialNumberController.handle)
-
+packageRouter.put("/:id",updatePackageController.handle)
 
 export { packageRouter }
