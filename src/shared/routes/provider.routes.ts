8d1856600bd874bc57b2ad8d96b5e979e73e7368
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 11:15:06 -0400

    feat: rota de criacao de fornecedor

--- src/shared/routes/provider.routes.ts
+++ src/shared/routes/provider.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { CreateProviderController } from "../../modules/Provider/useCases/CreateProviderUseCase/CreateProviderController";
 import { ListProviderController } from "../../modules/Provider/useCases/ListProviderUseCase/ListProviderController";
 
 
 const providerRouter = Router()
 
 const listProviderController = new ListProviderController()
+const createProviderController = new CreateProviderController()
 
 providerRouter.get("/",listProviderController.handle)
+providerRouter.post("/",createProviderController.handle)
 
 export { providerRouter }
