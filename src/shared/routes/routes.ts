af84dd2fcd302202842b6002ff112f4f0120103f
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 10:07:08 -0400

    feat: rota de fornecedores

--- src/shared/routes/routes.ts
+++ src/shared/routes/routes.ts
@@ -3,6 +3,7 @@ import { cpcRouter } from "./cpc.routes";
 import { customerRouter } from "./customer.routes";
 import { modelRouter } from "./model.routes";
 import { packageRouter } from "./package.routes";
+import { providerRouter } from "./provider.routes";
 import { userRouter } from "./user.routes";
 
 const router = Router();
@@ -13,6 +14,7 @@ router.use("/cpc", cpcRouter)
 router.use("/package", packageRouter)
 router.use("/customer", customerRouter)
 router.use("/user", userRouter)
+router.use("/provider",providerRouter)
 
 
 export {router}
