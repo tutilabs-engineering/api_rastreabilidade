7bda4ec60d6d2df7575b26eaddbedd608637846f
paulabssantos <paulablsantos7@gmail.com>
2023-03-01 08:09:16 -0400

    feat: rota de buscar embalagem pelo numero de serie

--- src/shared/routes/package.routes.ts
+++ src/shared/routes/package.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { FindBySerialNumberController } from "../../modules/Package/useCase/FindBySerialNumberUseCase/FindBySerialNumberController";
 
 
 const packageRouter = Router()
+const findBySerialNumberController = new FindBySerialNumberController()
 
-
+packageRouter.get("/:serial_number", findBySerialNumberController.handle)
 
 
 export { packageRouter }
