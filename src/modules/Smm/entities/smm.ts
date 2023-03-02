ab1cc7d566ea24284e8036f9e87f46cd57e1b74e
paulabssantos <paulablsantos7@gmail.com>
2023-03-02 13:38:41 -0400

    feat: entidade smm

---
+++ src/modules/Smm/entities/smm.ts
@@ -0,0 +1,14 @@
+class Smm{
+    id: string
+    username: string
+    matricula: string
+    fornecedor: string
+    statusDoFornecedor: string
+    descricao: string
+    serial_number: string
+    statusDaEmbalagem: string
+    modeloDaEmbalagem: string
+    statusDeMovimentacao: boolean
+    criadoEm: string
+    concluidoEm: string
+}
