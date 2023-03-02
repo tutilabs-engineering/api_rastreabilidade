8934b82a0ddb739413c70ba3f94b8aa12fba26c0
paulabssantos <paulablsantos7@gmail.com>
2023-03-02 08:35:53 -0400

    feat: rota de criacao de embalagem

--- src/shared/routes/package.routes.ts
+++ src/shared/routes/package.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { CreatePackageController } from "../../modules/Package/useCase/CreatePackageUseCase/CreatePackageController";
 import { FindBySerialNumberController } from "../../modules/Package/useCase/FindBySerialNumberUseCase/FindBySerialNumberController";
 import { UpdatePackageController } from "../../modules/Package/useCase/UpdatePackageUseCase/UpdatePackageController";
 
@@ -6,8 +7,10 @@ import { UpdatePackageController } from "../../modules/Package/useCase/UpdatePac
 const packageRouter = Router()
 const findBySerialNumberController = new FindBySerialNumberController()
 const updatePackageController = new UpdatePackageController()
+const createPackageController = new CreatePackageController()
 
 packageRouter.get("/:serial_number", findBySerialNumberController.handle)
 packageRouter.put("/:id",updatePackageController.handle)
+packageRouter.post("/",createPackageController.handle)
 
 export { packageRouter }
