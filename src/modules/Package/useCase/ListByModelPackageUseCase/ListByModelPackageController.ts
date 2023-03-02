47ea436556930012be088c6facc9a063baa559f9
paulabssantos <paulablsantos7@gmail.com>
2023-03-02 13:38:27 -0400

    feat: listagem contagem de embalagem por modelo e unidade

--- src/modules/Package/useCase/ListByModelPackageUseCase/ListByModelPackageController.ts
+++ src/modules/Package/useCase/ListByModelPackageUseCase/ListByModelPackageController.ts
@@ -6,17 +6,11 @@ class ListByModelPackageController {
 
     async handle(req: Request, res: Response){
 
-        const {model} = req.params
-        const { limit, take } = req.query
+        const { id } = req.params
+        const { limit = 0, take = 10 } = req.query
 
         const listByModelPackageUseCase = container.resolve(ListByModelPackageUseCase)
-        const embalagens = await listByModelPackageUseCase.execute({model}, {limit: Number(limit), take:  Number(take)})
-
-        // Instanciando o os serviços de embalagem
-        // const listByModelPackageServices = new ListByModelPackageServices()
-
-        // buscando as embalagem no banco
-        // const embalagens = await listByModelPackageServices.list(String(model))
+        const embalagens = await listByModelPackageUseCase.execute({model: id}, {limit: Number(limit), take:  Number(take)})
 
         return res.status(200).json(embalagens)
 
